85281e80c04784dc53da7d287b5b8692eecc6dbd
Emily Wells Jimenez and Rane Fields <emily+rane@>
2017-09-14 14:33:17 -0700

    add triangle classification logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,65 @@
+var classifyTriangle = function(side1, side2, side3) {
+  if (checkIsTriangle(side1, side2, side3)) {
+    if(checkIsEquilateral(side1, side2, side3)) {
+      return "equilateral";
+    } else if (checkIsIsosceles(side1, side2, side3)) {
+      return "isosceles";
+    } else {
+      return "scalene";
+    }
+  } else {
+    return "non";
+  }
+};
 
+
+
+var checkIsTriangle = function(side1, side2, side3) {
+  if (side1 + side2 < side3) {
+    return false;
+  }
+  else if (side2 + side3 < side1) {
+    return false;
+  }
+  else if (side1 + side3 < side2) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+var checkIsEquilateral = function(side1, side2, side3) {
+  if (side1 === side2 && side2 === side3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var checkIsIsosceles = function(side1, side2, side3) {
+  if (side1 === side2 && side2 !== side3) {
+    return true;
+  }
+  else if (side1 === side3 && side2 !== side3) {
+    return true;
+  }
+  else if (side2 === side3 && side1 !== side3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+$(document).ready(function() {
+  $("#triangle-data").submit(function(event) {
+    event.preventDefault();
+
+    var side1 = parseInt($("#side1").val());
+    var side2 = parseInt($("#side2").val());
+    var side3 = parseInt($("#side3").val());
+
+    alert("Your triangle is a " + classifyTriangle(side1, side2, side3) + " triangle.");
+
+  });
+});
